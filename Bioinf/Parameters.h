2a045057683dd2711c6be4b57ef6bb8b9b032fac
DominikMistera <DominikMistera@DESKTOP-CL3ESFE>
2020-05-07 18:17:08 +0200

    Adjusted parameters

--- Bioinf/Parameters.h
+++ Bioinf/Parameters.h
@@ -2,8 +2,8 @@
 
 static const int I = 100;
 static const int M = 20;
-static const float ALPHA = 2.0f;
+static const float ALPHA = 3.0f;
 static const float BETA = 2.0f;
 static const float GAMMA = 0.9f;
 static const float DELTA = 2.0f;
-static const float EPSILON = 2.0f;
+static const float EPSILON = 4.0f;
